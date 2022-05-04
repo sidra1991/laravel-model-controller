61dd8e45377b116479de42f54c51d6832de50c0e
anthony <diomede1991@libero.it>
2022-05-04 20:13:43 +0200

    basta.... finirò domani

--- esercizio/app/Http/Controllers/PageController.php
+++ esercizio/app/Http/Controllers/PageController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
     public function list() {
-        return view('listaLibri');
+        $libri = Movie::all();
+        //dd($libri);
+        return view('listaLibri',['libri'=>$libri]);
     }
 }
--- esercizio/routes/web.php
+++ esercizio/routes/web.php
@@ -13,8 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
 
-Route::get('/data', 'PageController@list')->name('Pagecontroller');
+Route::get('/', 'PageController@list')->name('Pagecontroller');
